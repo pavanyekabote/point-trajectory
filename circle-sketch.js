970d6ca1b57e986af819764802a311ce88904dfe
Pavan Yekabote <yekabotep@gmail.com>
2018-09-20 23:57:58 +0530

    Created multiple points to form a physical solar system

--- circle-sketch.js
+++ circle-sketch.js
@@ -1,9 +1,11 @@
+
+
 class mPoint{
     constructor(x,y)
     {
         this.x = x;
         this.y = y;
-        this.r = 50;
+        this.r = 8  ;
     }
 
     move(x,y)
@@ -26,7 +28,7 @@ class Square{
         this.cX = cX;
         this.cY = cY;
         this.stage = 1;
-        this.point = new mPoint(x,y);
+        this.point = new mPoint(cX, cY);
         this.x = cX;
         this.y = cY;
         this.s = s;
@@ -34,6 +36,12 @@ class Square{
         this.g = 0.01;
     }
 
+
+    setCenter(x,y)
+    {
+        this.cX = x-(this.s/2);
+        this.cY = y-(this.s/2);
+    }
 
     draw(){
 
@@ -47,8 +55,8 @@ class Square{
         this.v += this.g;
         if(this.stage == 1)
         {
-            
-            
+
+
             this.x+=this.v;
             if(this.x >= (this.cX) + this.s ){
                 this.x=(this.cX)+this.s;
@@ -58,7 +66,7 @@ class Square{
         }
         if(this.stage == 2)
         {
-           
+
             this.y += this.v;
             if( this.y >= this.cY + this.s ){
                 this.y = this.cY + this.s;
@@ -67,7 +75,7 @@ class Square{
         }
         if(this.stage == 3)
         {
-            
+
             this.x -= this.v;
             if( this.x <= this.cX ){
                 this.x = this.cX;
@@ -76,7 +84,7 @@ class Square{
         }
         if(this.stage == 4)
         {
-            
+
             this.y -= this.v;
             if(this.y <= this.cY){
                 this.y = this.cY;
@@ -84,7 +92,7 @@ class Square{
             }
         }
 
-      
+
     }
 
 
@@ -94,34 +102,41 @@ class Square{
 class Circle
 {
 
-    constructor(cx, cy, r)
+    constructor(cx, cy, r, d)
     {
         this.cX = cx;
         this.cY = cy;
         this.x = cx;
         this.y = cy;
         this.r = r;
-        this.point = new mPoint(x,y);
-        this.g = 0.1;
+        this.delta = d;
+        this.point = new mPoint(cx,cy);
+        this.g = 0.005;
         this.count = 0;
         this.v = 0;
         this.deg = 0;
     }
 
 
+    setCenter(x,y)
+    {
+        this.cX = x;
+        this.cY = y;
+    }
+
     move()
     {
-        
+
         this.v += this.g;
         this.deg=( this.count++ / 500 ) * this.v ;
-        this.x = Math.round( Math.cos(this.deg) * this.r ) ;
-        this.y = Math.round( Math.sin(this.deg) * this.r ) ;
+        this.x = Math.round( Math.cos(this.deg *  this.delta ) * this.r /  this.delta )  ;
+        this.y = Math.round( Math.sin(this.deg *  this.delta ) * this.r / this.delta ) ;
 
         fill(255);
 
         var mx = this.cX+ this.y;
         var my = this.cY+ this.x;
-      
+
         this.point.move(mx, my);
 
     }
@@ -137,95 +152,73 @@ function pointsDistance(p1,p2){
     return Math.sqrt((p1.x-p2.x) * (p1.x-p2.x) + (p1.y-p2.y) * (p1.y-p2.y));
 }
 
-var mW,mH, cW, cH;
-var p ,run=true, cP,mx,my, gravity=0.1, v=0;
-var cD = new Date();
-var x,y, nDegrees, deg=1;
+var  cW, cH, run=true;
 
 var sq, rD=50, cir;
 
 function setup() {
-    createCanvas(600,500);
+    createCanvas(600,600);
     mW = width;
     mH = height;
     cW = mW/2;
     cH = mH/2;
-    nDegrees = 360;
-    p = new mPoint(cW, cH);
-    cP = new mPoint(cW, cH);
+
+    //Initialize Shapes
     sq = new Square(cW-rD, cH - rD,  rD * 2);
     cir = new Circle(cW, cH, rD );
+    circles = [];
     background(0);
+
+    var nCircles = 9;
+    for(let i=0 ; i< nCircles; i++)
+        circles.push(new Circle(cH, cW, rD,  i  * (2 * Math.PI * rD / nCircles) / 150 ) );
 }
 
 
 
 
+/**
+ Author : Pavan Yekabote;
 
+*/
 function draw() {
     if(frameCount % 1 == 0 && run)
     {
 
 
+//              background(0);
+
+        for(let c =0 ; c < circles.length; c++ )
+        {
+//            fill(rand(255), rand(255), rand(255));
+            fill(255);
+            circles[c].draw();
+            circles[c].move();
+        }
+
+        //For Circle
+        // cir.draw();
+        // cir.move();
+
         //For Square
-        background(0);
-        fill(255);
-        //sq.draw();
-        //sq.move();
-
-        //fill(rand(255), rand(255), rand(255));
-        cir.draw();
-        cir.move();
-
-
-        //fill(255);
-      
-        /* p.draw();
-
-        //Move in circular form
-        
-        v += gravity;
-        deg=( frameCount / 500 ) * v ;
-        x = Math.round( Math.cos(deg) * rD ) ;
-        y = Math.round( Math.sin(deg) * rD ) ;
-        var d = Math.round(pointsDistance(p,cP));
-        fill(0);
-        rect(0,0,700,30);
-
-        fill(255,255,0);
-        text("Distance: "+d +" > degrees: "+deg+"  x:"+x+" y:"+y, 20,20);
-        stroke(255);
-
-        mx = cW-y;
-        my = cH-x;
-      
-        p.move(mx, my);
-        cP.draw();
-        cP.move(cW, cH);
-        */
-
-
+        // sq.draw();
+        // sq.move();
     }
 }
 
-
-function drawSquare(cX, cY, s)
-{
-   
-        
-
-}
 function mouseMoved(e)
 {
-  //  text(e.x+" -- "+e.y, 20, 50);
-   // cW = e.x;
-    //cH = e.y;
-    //background(0);
+    text(e.x+" -- "+e.y, 20, 50);
+    cir.setCenter(e.x, e.y);
+    sq.setCenter(e.x, e.y);
+    for(let i=0; i< circles.length; i++)
+    circles[i].setCenter(e.x, e.y);
+    background(0);
 }
 
 function keyPressed(e){
     if(e.keyCode == 65)
-      run = !run;
+    run = !run;
 }
 
 
